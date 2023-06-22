294eb9dc83fa46add9d3809d6c9ddd1ad532f299
MarcosVel <marcosvelosodev@gmail.com>
2023-06-22 19:17:45 -0300

    feat: my ads products

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,8 @@
-import { Flex, Text } from "native-base";
-import { SafeAreaView } from "react-native";
+import { Box, Center, Flex, Stack, Text, VStack } from "native-base";
+import { ArrowRight, Plus, Tag } from "phosphor-react-native";
+import { SafeAreaView, TouchableOpacity } from "react-native";
 import Button from "../components/Button";
 import UserAvatar from "../components/UserAvatar";
-import { Plus } from "phosphor-react-native";
 
 export default function Home() {
   return (
@@ -44,6 +44,48 @@ export default function Home() {
           leftIcon={<Plus color="#F7F7F8" size={18} />}
         />
       </Flex>
+
+      <VStack px={6}>
+        <Text fontFamily="body" color="gray.500" mb={3}>
+          Seus produtos anunciados para venda
+        </Text>
+        <Box
+          bg="rgba(100, 122, 199, 0.1)"
+          rounded={6}
+          px={4}
+          py={3}
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Center flexDirection="row">
+            <Tag size={22} color="#364D9D" />
+
+            <Stack ml={4}>
+              <Text
+                fontFamily="heading"
+                color="gray.600"
+                fontSize="lg"
+                lineHeight="xs"
+              >
+                4
+              </Text>
+              <Text fontFamily="body" color="gray.600" fontSize="xs">
+                anúncios ativos
+              </Text>
+            </Stack>
+          </Center>
+
+          <TouchableOpacity>
+            <Center flexDirection="row">
+              <Text fontFamily="heading" color="blue.800" fontSize="xs" mr={2}>
+                Meus anúncios
+              </Text>
+              <ArrowRight size={16} color="#364D9D" />
+            </Center>
+          </TouchableOpacity>
+        </Box>
+      </VStack>
     </SafeAreaView>
   );
 }
