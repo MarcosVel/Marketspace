6186e4e468121e43e2a77686026063d3ae3d2be9
MarcosVel <marcosvelosodev@gmail.com>
2023-08-19 01:53:10 -0300

    feat: form validation and function to create ad

--- mobile/src/screens/CreateAd.tsx
+++ mobile/src/screens/CreateAd.tsx
@@ -1,3 +1,4 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigation } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
 import {
@@ -5,6 +6,7 @@ import {
   Checkbox,
   FlatList,
   Flex,
+  FormControl,
   HStack,
   Image,
   KeyboardAvoidingView,
@@ -14,12 +16,17 @@ import {
   Text,
   TextArea,
   VStack,
+  useToast,
 } from "native-base";
 import { ArrowLeft, Plus, X } from "phosphor-react-native";
 import { useLayoutEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { SafeAreaView, TouchableOpacity } from "react-native";
+import * as yup from "yup";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import { AppNavigationProps } from "../routes/app.routes";
+import api from "../services/api";
 
 type PhotoFileProps = {
   uri: string;
@@ -27,9 +34,52 @@ type PhotoFileProps = {
   name: string;
 };
 
+type FormDataProps = {
+  name: string;
+  description: string;
+  is_new: string;
+  price: number;
+  accept_trade: boolean;
+  payment_methods: string[];
+};
+
+const createAdSchema = yup.object({
+  name: yup.string().required("O nome do produto é obrigatório"),
+  description: yup.string().required("A descrição do produto é obrigatória"),
+  is_new: yup.string().required("É obrigatório informar o estado do produto"),
+  price: yup
+    .number()
+    .required("O preço do produto é obrigatório")
+    .typeError("O preço do produto é obrigatório") // pice must be a `number` type, but the final value was: `NaN` (cast from the value `""`)
+    .moreThan(0, "O preço do produto deve ser maior que 0"),
+  accept_trade: yup
+    .boolean()
+    .required("É obrigatório informar se aceita troca"),
+  payment_methods: yup
+    .array()
+    .min(1, "É obrigatório informar os meios de pagamento aceitos")
+    .required("É obrigatório informar os meios de pagamento aceitos"),
+});
+
 export default function CreateAd() {
-  const navigation = useNavigation();
+  const toast = useToast();
+  const navigation = useNavigation<AppNavigationProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    defaultValues: {
+      name: "",
+      description: "",
+      is_new: "",
+      accept_trade: false,
+      payment_methods: [],
+    },
+    resolver: yupResolver(createAdSchema),
+  });
   const [images, setImages] = useState<PhotoFileProps[]>([]);
+  const [groupValues, setGroupValues] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -50,7 +100,7 @@ export default function CreateAd() {
         aspect: [1, 1],
         quality: 1,
         allowsMultipleSelection: true,
-        selectionLimit: 3,
+        selectionLimit: 3 - images.length,
         orderedSelection: true,
       });
 
@@ -88,6 +138,59 @@ export default function CreateAd() {
       );
     } catch (error) {
       console.log("error on handleRemoveImage:", error);
+    }
+  }
+
+  async function handleAdCreation({
+    name,
+    description,
+    is_new,
+    price,
+    accept_trade,
+    payment_methods,
+  }: FormDataProps) {
+    try {
+      if (images.length < 1) {
+        return toast.show({
+          title: "Selecione pelo menos uma imagem",
+        });
+      }
+
+      const { data } = await api.post("/products", {
+        name,
+        description,
+        is_new: is_new === "new" ? true : false,
+        price,
+        accept_trade,
+        payment_methods,
+      });
+
+      const formData = new FormData();
+      formData.append("product_id", data.id);
+      // Convert and append each image to the formData
+      images.forEach((image) => {
+        const blob = new Blob([image.uri], { type: image.type });
+        const file = new File([blob], image.name);
+        formData.append("images", file);
+      });
+
+      await api
+        .post("/products/images", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(() => {
+          toast.show({
+            title: "Anúncio criado com sucesso!",
+          });
+          navigation.navigate("myAds");
+        })
+        .catch((error) => {
+          console.log("error to add product images:", error);
+        });
+    } catch (error) {
+      console.log("error on handleAdCreation:", error);
     }
   }
 
@@ -155,8 +258,10 @@ export default function CreateAd() {
                       alignItems: "center",
                       justifyContent: "center",
                       backgroundColor: "#D9D8DA",
+                      display: images.length > 2 ? "none" : "flex",
                     }}
                     onPress={handleProductsImages}
+                    disabled={images.length > 2}
                   >
                     <Plus size={24} color="#9F9BA1" />
                   </TouchableOpacity>
@@ -175,59 +280,104 @@ export default function CreateAd() {
                 Sobre o produto
               </Text>
 
-              <Input placeholder="Título do anúncio" />
-
-              <TextArea
-                placeholder="Descrição do produto"
-                autoCompleteType={true}
-                py={3}
-                px={4}
-                h={160}
-                bg="gray.100"
-                rounded={6}
-                borderWidth={0}
-                fontSize="md"
-                fontFamily="body"
-                placeholderTextColor="gray.400"
-                color="gray.600"
-                _focus={{
-                  bg: "gray.100",
-                }}
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Título do anúncio"
+                    value={value}
+                    onChangeText={onChange}
+                    errorMessage={errors.name?.message}
+                  />
+                )}
               />
 
-              <Radio.Group name="productName">
-                <HStack alignItems="center" space={6}>
-                  <Radio
-                    value="1"
-                    bg="gray.200"
-                    _checked={{
-                      borderColor: "blue.400",
-                    }}
-                    _icon={{
-                      color: "blue.400",
-                    }}
-                  >
-                    <Text fontFamily="body" fontSize="md" color="gray.600">
-                      Produto novo
-                    </Text>
-                  </Radio>
+              <Controller
+                control={control}
+                name="description"
+                render={({ field: { onChange, value } }) => (
+                  <FormControl isInvalid={!!errors.description?.message}>
+                    <TextArea
+                      placeholder="Descrição do produto"
+                      autoCompleteType={true}
+                      py={3}
+                      px={4}
+                      h={160}
+                      bg="gray.100"
+                      rounded={6}
+                      borderWidth={0}
+                      fontSize="md"
+                      fontFamily="body"
+                      placeholderTextColor="gray.400"
+                      color="gray.600"
+                      _focus={{
+                        bg: "gray.100",
+                      }}
+                      value={value}
+                      onChangeText={onChange}
+                      maxLength={300}
+                    />
+                    <FormControl.ErrorMessage _text={{ color: "red.400" }}>
+                      {errors.description?.message}
+                    </FormControl.ErrorMessage>
+                  </FormControl>
+                )}
+              />
 
-                  <Radio
-                    value="2"
-                    bg="gray.200"
-                    _checked={{
-                      borderColor: "blue.400",
-                    }}
-                    _icon={{
-                      color: "blue.400",
-                    }}
-                  >
-                    <Text fontFamily="body" fontSize="md" color="gray.600">
-                      Produto usado
-                    </Text>
-                  </Radio>
-                </HStack>
-              </Radio.Group>
+              <Controller
+                control={control}
+                name="is_new"
+                render={({ field: { onChange, value } }) => (
+                  <FormControl isInvalid={!!errors.is_new?.message}>
+                    <Radio.Group
+                      name="productName"
+                      onChange={onChange}
+                      // onChange={(nextValue) => onChange(nextValue)}
+                      value={value}
+                      flexDirection="row"
+                      alignContent="center"
+                      style={{
+                        gap: 24,
+                      }}
+                    >
+                      <Radio
+                        value="new"
+                        bg="gray.200"
+                        _checked={{
+                          borderColor: "blue.400",
+                        }}
+                        _icon={{
+                          color: "blue.400",
+                        }}
+                      >
+                        <Text fontFamily="body" fontSize="md" color="gray.600">
+                          Produto novo
+                        </Text>
+                      </Radio>
+
+                      <Radio
+                        value="used"
+                        bg="gray.200"
+                        _checked={{
+                          borderColor: "blue.400",
+                        }}
+                        _icon={{
+                          color: "blue.400",
+                        }}
+                      >
+                        <Text fontFamily="body" fontSize="md" color="gray.600">
+                          Produto usado
+                        </Text>
+                      </Radio>
+                    </Radio.Group>
+
+                    <FormControl.ErrorMessage _text={{ color: "red.400" }}>
+                      {errors.is_new?.message}
+                    </FormControl.ErrorMessage>
+                  </FormControl>
+                )}
+              />
             </VStack>
 
             <VStack px={6} space={4} mt={8}>
@@ -241,16 +391,30 @@ export default function CreateAd() {
                 Venda
               </Text>
 
-              <Input
-                placeholder="Valor do produto"
-                keyboardType="numeric"
-                InputLeftElement={
-                  <Text fontSize="md" color="gray.700" fontFamily="body" ml={4}>
-                    R$
-                  </Text>
-                }
-                px={0}
-                pl={2}
+              <Controller
+                control={control}
+                name="price"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Valor do produto"
+                    keyboardType="decimal-pad"
+                    InputLeftElement={
+                      <Text
+                        fontSize="md"
+                        color="gray.700"
+                        fontFamily="body"
+                        ml={4}
+                      >
+                        R$
+                      </Text>
+                    }
+                    px={0}
+                    pl={2}
+                    value={value}
+                    onChangeText={onChange}
+                    errorMessage={errors.price?.message}
+                  />
+                )}
               />
             </VStack>
 
@@ -258,89 +422,140 @@ export default function CreateAd() {
               <Text fontFamily="heading" color="gray.600" mb={3}>
                 Aceita troca?
               </Text>
-              <Switch
-                size="md"
-                offTrackColor="gray.300"
-                onTrackColor="blue.400"
-                mb={4}
+              <Controller
+                control={control}
+                name="accept_trade"
+                render={({ field: { onChange, value } }) => (
+                  <Switch
+                    size="md"
+                    offTrackColor="gray.300"
+                    onTrackColor="blue.400"
+                    mb={4}
+                    value={value}
+                    onToggle={onChange}
+                  />
+                )}
               />
 
               <Text fontFamily="heading" color="gray.600" mb={3}>
                 Meios de pagamento aceitos
               </Text>
               <VStack space={2}>
-                <Checkbox
-                  value="boleto"
-                  _checked={{
-                    bg: "blue.400",
-                    borderColor: "blue.400",
-                  }}
-                  borderColor="gray.300"
-                >
-                  <Text fontFamily="body" fontSize="md" color="gray.600">
-                    Boleto
-                  </Text>
-                </Checkbox>
+                <Controller
+                  control={control}
+                  name="payment_methods"
+                  render={({ field: { onChange, value } }) => (
+                    <FormControl isInvalid={!!errors.payment_methods?.message}>
+                      <Checkbox.Group
+                        // onChange={setGroupValues}
+                        // value={groupValues}
+                        onChange={onChange}
+                        value={value}
+                      >
+                        <Checkbox
+                          value="boleto"
+                          _checked={{
+                            bg: "blue.400",
+                            borderColor: "blue.400",
+                          }}
+                          borderColor="gray.300"
+                        >
+                          <Text
+                            fontFamily="body"
+                            fontSize="md"
+                            color="gray.600"
+                          >
+                            Boleto
+                          </Text>
+                        </Checkbox>
 
-                <Checkbox
-                  value="pix"
-                  _checked={{
-                    bg: "blue.400",
-                    borderColor: "blue.400",
-                  }}
-                  borderColor="gray.300"
-                >
-                  <Text fontFamily="body" fontSize="md" color="gray.600">
-                    Pix
-                  </Text>
-                </Checkbox>
+                        <Checkbox
+                          value="pix"
+                          _checked={{
+                            bg: "blue.400",
+                            borderColor: "blue.400",
+                          }}
+                          borderColor="gray.300"
+                        >
+                          <Text
+                            fontFamily="body"
+                            fontSize="md"
+                            color="gray.600"
+                          >
+                            Pix
+                          </Text>
+                        </Checkbox>
 
-                <Checkbox
-                  value="dinheiro"
-                  _checked={{
-                    bg: "blue.400",
-                    borderColor: "blue.400",
-                  }}
-                  borderColor="gray.300"
-                >
-                  <Text fontFamily="body" fontSize="md" color="gray.600">
-                    Dinheiro
-                  </Text>
-                </Checkbox>
+                        <Checkbox
+                          value="cash"
+                          _checked={{
+                            bg: "blue.400",
+                            borderColor: "blue.400",
+                          }}
+                          borderColor="gray.300"
+                        >
+                          <Text
+                            fontFamily="body"
+                            fontSize="md"
+                            color="gray.600"
+                          >
+                            Dinheiro
+                          </Text>
+                        </Checkbox>
 
-                <Checkbox
-                  value="credit"
-                  _checked={{
-                    bg: "blue.400",
-                    borderColor: "blue.400",
-                  }}
-                  borderColor="gray.300"
-                >
-                  <Text fontFamily="body" fontSize="md" color="gray.600">
-                    Cartão de Crédito
-                  </Text>
-                </Checkbox>
+                        <Checkbox
+                          value="card"
+                          _checked={{
+                            bg: "blue.400",
+                            borderColor: "blue.400",
+                          }}
+                          borderColor="gray.300"
+                        >
+                          <Text
+                            fontFamily="body"
+                            fontSize="md"
+                            color="gray.600"
+                          >
+                            Cartão de Crédito
+                          </Text>
+                        </Checkbox>
 
-                <Checkbox
-                  value="deposit"
-                  _checked={{
-                    bg: "blue.400",
-                    borderColor: "blue.400",
-                  }}
-                  borderColor="gray.300"
-                >
-                  <Text fontFamily="body" fontSize="md" color="gray.600">
-                    Depósito Bancário
-                  </Text>
-                </Checkbox>
+                        <Checkbox
+                          value="deposit"
+                          _checked={{
+                            bg: "blue.400",
+                            borderColor: "blue.400",
+                          }}
+                          borderColor="gray.300"
+                        >
+                          <Text
+                            fontFamily="body"
+                            fontSize="md"
+                            color="gray.600"
+                          >
+                            Depósito Bancário
+                          </Text>
+                        </Checkbox>
+                      </Checkbox.Group>
+
+                      <FormControl.ErrorMessage _text={{ color: "red.400" }}>
+                        {errors.payment_methods?.message}
+                      </FormControl.ErrorMessage>
+                    </FormControl>
+                  )}
+                />
               </VStack>
             </VStack>
 
             <Box pt={5} px={6} pb={3} bg="gray.100" w="full">
               <HStack space={3}>
-                <Button title="Cancelar" />
+                <Button title="Cancelar" onPress={() => navigation.goBack()} />
 
-                <Button title="Avançar" variant="dark" />
+                <Button
+                  title="Avançar"
+                  variant="dark"
+                  onPress={handleSubmit(handleAdCreation)}
+                />
               </HStack>
             </Box>
           </KeyboardAvoidingView>
